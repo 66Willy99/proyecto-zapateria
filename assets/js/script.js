73efbdd371bd4616464fa3c8067d5d78aeb81c77
Guillermo Mercado <guillermo.mercado26@gmail.como>
2024-04-26 21:14:49 -0400

    intro javascript y creacion contacto.html

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+// let numero1 = 0;
+// let numero2 = 0;
+
+//let resultado = 0;
+
+//let error1 = document.getElementById("error1");
+//let error2 = document.getElementById("error2");
+
+//let btnSumar= document.getElementById("Sumar");
+//btnSumar.addEventListener("click", function(){
+//    error1.innerHTML = "";
+//    error2.innerHTML = "";
+//    numero1 = document.getElementById("numero1").value;
+//    numero2 = document.getElementById("numero2").value;
+//    if(numero1===""){
+//        Swal.fire({
+//            title: "Falta información!",
+//            text: "Debes agregar el primer numero",
+//            icon: "warning"
+//          });
+//        return;
+//    }
+//    numero1 = parseInt(numero1);
+//
+//    if (numero2===""){
+//        Swal.fire({
+//            title: "Falta información!",
+//            text: "Debes agregar el segundo numero",
+//            icon: "warning"
+//          });
+//        return;
+//    }
+//    numero2 = parseInt(numero2);
+//    resultado = numero1 + numero2;
+//    alert("Sumaste: "+ numero1 +" + " + numero2 +"= "+ resultado)
+//});
